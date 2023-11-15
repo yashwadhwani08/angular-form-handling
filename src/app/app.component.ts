18f88e206944826ee19285a5cfa2724d208a09d0
yashwadhwani08 <yashwadhwani08@gmail.com>
2023-11-15 20:17:01 +0530

    getting access to the JS form object created by Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  onSubmit(form: NgForm) {
+    console.log('Submiited!');
+    console.log(form);
+    // The vaue in this form object contains the the names we set and user-input values for those inputs as key-value pairs.
   }
 }
