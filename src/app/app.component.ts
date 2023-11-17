71cc90af923409d0369ce2311fd1ae4a3fcdf8fc
yashwadhwani08 <yashwadhwani08@gmail.com>
2023-11-17 19:28:48 +0530

    added radio button and working with the form-control on it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
   defaultQuestion: string = 'pet';
   answer: string = '';
+  genders = ['male', 'female'];
 
   suggestUserName() {
     const suggestedName = 'Superuser';
