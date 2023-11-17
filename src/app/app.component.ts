19b12a5b3fef3b0fa605cafe0cd73a1c71cdf05e
yashwadhwani08 <yashwadhwani08@gmail.com>
2023-11-17 12:57:44 +0530

    using ngModel with property binding to set some initialdefault value for the dropdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
+  defaultQuestion: string = 'pet';
 
   suggestUserName() {
     const suggestedName = 'Superuser';
