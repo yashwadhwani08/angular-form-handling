2bc8a03b3384f11397fefbe67f17a3578375d8da
yashwadhwani08 <yashwadhwani08@gmail.com>
2023-11-17 13:30:09 +0530

    using ngModel with two-way databinding to react to the changes in the input instantly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
   defaultQuestion: string = 'pet';
+  answer: string = '';
 
   suggestUserName() {
     const suggestedName = 'Superuser';
