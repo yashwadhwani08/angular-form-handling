4f181f58d69cbc9e6262201bf43c5221068c1f4e
yashwadhwani08 <yashwadhwani08@gmail.com>
2023-11-19 13:37:41 +0530

    using patchValue method to only change the control we want and not rest of the controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,14 +18,23 @@ export class AppComponent {
     // to the setValue method, you need to pass the JS object 'exactly representing our form
 
     // this approach has downside: if we already have something entered in the form in the rest of the inputs, this approach would override all other content. Hence not necessarily, the best approach. But, this can be helpful when we to set the values of all controls in one go with one convenient command (setValue).
-    this.signupForm.setValue({
+
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: '',
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male',
+    // });
+
+    // better approach to access the form
+    // use form method on the ngForm object, on it we have patchValue method. A JS object has to be passed, here we can override specific controls, hence since we needed to change username which is inside userData, hence the object key starts as userData and username passed (not email, neither any other form-control)
+    this.signupForm.form.patchValue({
       userData: {
-        username: suggestedName,
-        email: '',
-      },
-      secret: 'pet',
-      questionAnswer: '',
-      gender: 'male',
+        username: suggestedName
+      }
     });
   }
 
